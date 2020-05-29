52edd092b35f271514d5c21dcdc95a9c453b1d37
Joaquin Diaz <joacod@gmail.com>
2020-05-29 19:39:04 +0200

    chalk, debug and morgan added

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require("express");
+var chalk = require("chalk");
+var debug = require("debug")("app");
+var morgan = require("morgan");
 
 var app = express();
+
+app.use(morgan("tiny"));
 
 app.get("/", function (req, resp) {
   resp.send("Hello Node.js Playground API");
 });
 
 app.listen(8080, function () {
-  console.log("Listening in port 8080");
+  debug(`Listening on port ${chalk.green("8080")}`);
 });
